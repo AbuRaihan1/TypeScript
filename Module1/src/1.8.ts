25143968ebc27127ff649b2d579864cb8bd3be74
Abu Raihan <raihanahmed01973@gmail.com>
2024-04-04 20:53:09 +0600

    object desttucturing

---
+++ Module1/src/1.8.ts
@@ -0,0 +1,19 @@
+{
+  // object destructuring
+
+  const person = {
+    name: {
+      firstName: "Abu",
+      middleName: "Raihan",
+      lastName: "Morol",
+    },
+    age: 20,
+    contactNo: 1916891765,
+    address: "Hazibag, Gazipiur Sadar, Gazipur",
+  };
+
+  const {
+    contactNo,
+    name: { middleName },
+  } = person;
+}
