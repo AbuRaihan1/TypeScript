cd74a2f62cc6a6fa50f41ae484fa17f2a800f5eb
Abu Raihan <raihanahmed01973@gmail.com>
2024-04-04 08:31:56 +0600

    learned spread operator

---
+++ Module1/src/1.7.ts
@@ -0,0 +1,21 @@
+{
+  // spread operator
+  const mentors1 = {
+    typeScript: "Raihan",
+    nextJS: "Tanmoy",
+    redux: "Sumit Saha",
+  };
+
+  const mentors2 = {
+    html: "Ishan",
+    css: "Jhankar vai",
+  };
+
+  const mentors = {
+    ...mentors1,
+    ...mentors2,
+  };
+
+
+
+}
