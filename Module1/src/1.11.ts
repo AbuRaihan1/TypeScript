d1cd2a1d5a3f414041994232a697c3d7030b40f3
Abu Raihan <raihanahmed01973@gmail.com>
2024-04-05 18:44:57 +0600

    learned nullish operator

---
+++ Module1/src/1.11.ts
@@ -0,0 +1,10 @@
+{
+  // nullish coalescing
+  // nullish coalescing হলো, null এবং undefined এর উপর ভিত্তি করে কন্ডিশেনালি কাজ করে। অনেকটা ternery অপারেটর এর মতো। তবে, টার্নারি অপারেটর falsy value নিয়ে কাজ করে। আর nullish coalescing শুধু null এবং undefined নিয়ে কাজ করে।
+
+  const foo = null ?? "default value";
+  console.log(foo); // Output: 'default value'
+
+  const bar = "some value" ?? "default value";
+  console.log(bar); // Output: 'some value'
+}
